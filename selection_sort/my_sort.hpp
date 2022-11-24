d319ba577d440560d2029e5a9d5fac28222e5732
Agustin BOUCHON <agub200020@hotmail.fr>
2022-11-24 11:39:09 +0100

    selection sort file

---
+++ selection_sort/my_sort.hpp
@@ -0,0 +1,19 @@
+// Change me instead
+// All occurences of XXX need to be replaced with
+// something more meaningful
+
+
+template<class CONT>
+void my_selection_sort( CONT begin,CONT end) {
+    for (auto i = begin; i != end; ++i) {
+        auto min = i;
+        for (auto j = i; j != end; ++j) {
+            if (*j < *min) {
+                min = j;
+            }
+        }
+        auto tmp = *i;
+        *i = *min;
+        *min = tmp;
+    }
+}
